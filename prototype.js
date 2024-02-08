785c944334466ccac8d5e1c471259bc3c2f93358
leedongkyu <leedong0225@icloud.com>
2024-02-08 20:00:56 +0900

    prototype

--- prototype.js
+++ prototype.js
@@ -15,6 +15,10 @@ class Book {
         return this.autor;
     }
 
+    getTitle(){
+        return this.title;
+    }
+
     toString(){
         return `(${this.autor}  ${this.title})`;
     }
@@ -42,15 +46,25 @@ class BookShelf {
     toString(){
         return this.shelf.toString();
     }
+    clone(){
+        const another  = new BookShelf();
+        this.shelf.forEach((e,i)=>{
+            another.addBook(new Book(e.getAutor(),e.getTitle()));
+        });
+
+        return another;
+    }
 }
 
+
 const bookShelf = new BookShelf();
+
 bookShelf.addBook(new Book("lee", "자료구조"));
 bookShelf.addBook(new Book("kim", "c언어"));
 bookShelf.addBook(new Book("park", "자바"));
-
+console.log(bookShelf.getShelf()[0].getAutor());
 const tmpBookShelf = Object.create(bookShelf);// 프로토타입으로 인스턴스를 복사를 한다.
+console.log(`첫번째 복사방식 ${tmpBookShelf}`);
+const secondBookShelf = bookShelf.clone();
+console.log(`두번째 복사방식 ${secondBookShelf}`);
 
-tmpBookShelf.setShelf([]);
-tmpBookShelf.addBook(new Book('json','crazy'));
-console.log(`복제전 ${tmpBookShelf.toString()},복제${bookShelf.toString()}`);
